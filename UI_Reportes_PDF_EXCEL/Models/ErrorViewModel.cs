706283cb9fefcd46fbd76ad755b2f7f31bc53df1
Nestor.Adonay.Rodriguez.Alberto@gmail.com <nestor.adonay.rodriguez.alberto@gmail.com>
2024-09-02 16:02:06 -0600

    Proyecto Terminado

---
+++ UI_Reportes_PDF_EXCEL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UI_Reportes_PDF_EXCEL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
